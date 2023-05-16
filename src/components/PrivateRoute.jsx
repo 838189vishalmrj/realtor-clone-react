40572de8965a0212ac35378b1d512d55273b79c1
838189vishalmrj <vishalvishwakarma101mrj@gmail.com>
2023-05-16 17:12:12 +0530

    create the spinner component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router";
 import { Outlet } from "react-router-dom";
+import Spinner from "./Spinner";
 import useAuthStatus from "../hooks/useAuthStatus";
 export default function PrivateRoute() {
     const {loggedIn,checkingStatus} = useAuthStatus();
     if(checkingStatus){
-        return <h1>Loading</h1>
+        return <Spinner/>
     }
   return (
     loggedIn?<Outlet /> :<Navigate to={'/sign-in'}/>
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from '../assets/svg/spinner.svg'
+
+export default function Spinner() {
+  return (
+    <div className='bg-black bg-opacity-50 flex justify-center items-center fixed top-0 bottom-0 left-0 right-0 z-50'>
+        <div>
+            <img src={spinner} alt="Loading..." className='h-24' />
+        </div>
+    </div>
+  )
+}
